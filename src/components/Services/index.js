dbbca0480384b5f5aeb01c3490a70958425c12a3
piyushpp07 <59327328+piyushpp07@users.noreply.github.com>
2021-08-19 14:56:21 +0530

    dore

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -27,15 +27,17 @@ export default function Services() {
     }
     return (
         <div style={styles.main}>
-            <div style={styles.text}> Hello Everyone</div>
-            Enter The amount you want to donate
-            <form>
+            <h1>Your donations will be very helpful for the foundation </h1>
+            <div style={styles.content}>As India tries to stay afloat amidst the ‘second wave’ of the pandemic, our poor and vulnerable elderly face the highest risk. Countless have lost their lives and many disadvantaged elders were left starving due to loss of their livelihood. At such a time, it is critical that they get vaccinated immediately and safeguard themselves from the fatality of this deadly disease. As the pandemic makes inroads deeper into rural India, we need to ensure that the poor elderly & their families have food to eat, so they can maintain their nutrition and immunity levels and do not become more susceptible to the Covid 19 virus. Many are physically frail with chronic conditions, living alone and have no one to turn to. Your contribution can help an elder survive and bring hope during this uncertain time.</div>
+
+            <h3> Enter the amount you want to donate :</h3>
+            <form style={styles.from}>
                 <label>
-                    Amount :
-                    <input type="text" name="name" value={amount}
+                    <input style={styles.inp} type="text" name="name" value={amount}
                         onChange={(e) => setAmount(e.target.value)} />
                 </label>
             </form>
+
             <StripeCheckout stripeKey='pk_test_51JObFKSAm54TGSWjZQQVnpytQbBKaz7MqR7ewLtoeqZSsO9SZUl7n3ZZm3zEYV3sYmQnZaVbzZCttT3in6KJTxKS00lJalhL2a'
                 token={handleToken}
                 amount={amount * 100}
@@ -49,6 +51,14 @@ export default function Services() {
 
 }
 const styles = {
+    content: {
+        marginTop: '0',
+        width: '92%'
+    },
+    from: {
+        padding: '3px'
+    },
+
     main: {
         marginTop: '5em',
         height: '50em',
@@ -56,6 +66,14 @@ const styles = {
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundImage: `url("https://images.unsplash.com/photo-1611689102192-1f6e0e52df0a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2005&q=80")`,
+    },
+    inp: {
+        borderRadius: "8%",
+        borderColor: "#F2D200"
     }
 }
